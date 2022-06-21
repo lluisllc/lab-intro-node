b67c9d701d6b7ba16725affefd698119d72df900
lluisllc <lluis.llacera@gmail.com>
2022-06-21 17:03:10 +0200

    Final
    Incluido bonus

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
 
-  max() {}
+  get(pos) {
+    if (pos > this.items.length - 1) throw new Error('OutOfBounds');
+    else return this.items[pos];
+  }
 
-  min() {}
+  max() {
+    if (this.items.length === 0) throw new Error('EmptySortedList');
+    else return this.items.slice(-1)[0];
+  }
 
-  sum() {}
+  min() {
+    if (this.items.length === 0) throw new Error('EmptySortedList');
+    else return this.items[0];
+  }
 
-  avg() {}
+  sum() {
+    let total = 0;
+    for (let i = 0; i < this.items.length; i++) {
+      total = total + this.items[i];
+    }
+    return total;
+  }
+
+  avg() {
+    if (this.items.length === 0) throw new Error('EmptySortedList');
+    let total = 0;
+    for (let i = 0; i < this.items.length; i++) {
+      total = total + this.items[i]
+    }
+    return total / this.items.length;
+  }
 }
+
 
 module.exports = SortedList;
